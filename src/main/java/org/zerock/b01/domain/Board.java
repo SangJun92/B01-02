84ca09ebade81b639938f7f4f6a5a3032f0902ae
SangJun92 <nice92x@naver.com>
2024-05-08 16:40:46 +0900

    B01 Board image set 확인

--- src/main/java/org/zerock/b01/domain/Board.java
+++ src/main/java/org/zerock/b01/domain/Board.java
@@ -3,12 +3,15 @@ package org.zerock.b01.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
+@ToString(exclude = "imageSet")
 public class Board extends BaseEntity{
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,8 +23,11 @@ public class Board extends BaseEntity{
   @Column(length = 50, nullable = false)
   private String writer;
 
-  // 양방향을 한다면,
+  @OneToMany
+  @Builder.Default
+  private Set<BoardImage> imageSet = new HashSet<>();
 
+  // 양방향을 한다면,
 
   public void change(String title, String content){
     this.title = title;
